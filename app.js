128ea949fe51549867c8c1cd881117d19fa0992f
HLIAS <spiridonvrisis@gmail.com>
2019-11-11 20:04:15 +0200

    add producs json calculate average min max sum

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
 const chalk = require('chalk');
+const products = require("./products.json");
 
 console.log(chalk.bgBlue("Hello world"));
+
+console.log("to katastima mas exei " + products.length + (" proionta"));
+
+for (let p of products) {
+
+
+    if (p.sale) {
+        console.log(p.name + " " + chalk.green(p.sale))
+    }
+    else {
+        console.log(p.name + " " + p.price);
+    }
+}
+let sum = 0;
+let max = 0;
+let min = products[0].price;
+
+for (let p of products) {
+    sum = sum + p.price
+}
+for (let p of products) {
+    if (p.price > max) {
+        max = p.price;
+    }
+    if (p.price < min) {
+        min = p.price
+
+
+    }
+}
+
+
+let avg = sum / products.length;
+console.log(sum);
+console.log(avg);
+console.log(max);
+console.log(min);
